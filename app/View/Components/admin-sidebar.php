593d0bd703e1a9d60ae7567aef4dbc85927d7fa1
azmi0119 <sheikhabdullah0119@gmail.com>
2025-09-19 11:02:06 +0530

    formatting

--- app/View/Components/admin-sidebar.php
+++ app/View/Components/admin-sidebar.php
@@ -4,7 +4,7 @@ namespace App\View\Components;
 
 use Illuminate\View\Component;
 
-class admin-sidebar extends Component
+class AdminSidebar extends Component
 {
     /**
      * Create a new component instance.
